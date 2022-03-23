eb6b633b49f268daaf4479ec9c9a4b7ee5565590
RyuChigon <fbclrhs10@gmail.com>
2022-03-23 21:11:44 +0900

    Add BIND

--- app/kens/TCPAssignment.cpp
+++ app/kens/TCPAssignment.cpp
@@ -26,13 +26,10 @@ void TCPAssignment::initialize() {}
 
 void TCPAssignment::finalize() {}
 
-struct sock_info {
-};
-
 struct sock_table_item {
   int pid;
   int fd;
-  struct sock_info* sock_info;
+  struct sockaddr_in* sockaddr;
 };
 
 std::vector<sock_table_item*> sock_table;
@@ -59,12 +56,15 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid,
   case SOCKET: {
     // this->syscall_socket(syscallUUID, pid, std::get<int>(param.params[0]),
     //                      std::get<int>(param.params[1]));
-    struct sock_info* sock_info = (struct sock_info*) malloc(sizeof(struct sock_info));
     struct sock_table_item* sock_table_item = (struct sock_table_item*) malloc(sizeof(struct sock_table_item));
+    if (sock_table_item == NULL) {
+      printf("Error: can't allocate memory(sock_table_item)\n");
+      returnSystemCall(syscallUUID, -1);
+    }
 
     fd = createFileDescriptor(pid);
 
-    sock_table_item->sock_info = sock_info;
+    sock_table_item->sockaddr = NULL;
     sock_table_item->fd = fd;
     sock_table_item->pid = pid;
     sock_table.push_back(sock_table_item);
@@ -82,7 +82,8 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid,
 
     removeFileDescriptor(pid, fd);
 
-    free(sock_table_item->sock_info);
+    if (sock_table_item->sockaddr != NULL)
+      free(sock_table_item->sockaddr);
     free(sock_table_item);
     sock_table.erase(sock_table_item_itr);
 
@@ -115,12 +116,67 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid,
     //     static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
     //     static_cast<socklen_t *>(std::get<void *>(param.params[2])));
     break;
-  case BIND:
+  case BIND: {
     // this->syscall_bind(
     //     syscallUUID, pid, std::get<int>(param.params[0]),
     //     static_cast<struct sockaddr *>(std::get<void *>(param.params[1])),
     //     (socklen_t)std::get<int>(param.params[2]));
+    const in_addr_t NL_INADDR_ANY = htonl(INADDR_ANY);
+    int param_fd = std::get<int>(param.params[0]);
+    struct sockaddr_in *param_addr = 
+      (struct sockaddr_in *)(static_cast<struct sockaddr *>(std::get<void *>(param.params[1])));
+    sock_table_item_itr findItr;
+    sock_table_item_itr itr;
+    bool isFind = false;
+
+    for (itr = sock_table.begin(); itr != sock_table.end(); ++itr) {
+      if ((*itr)->pid == pid) {
+        /*
+          Consider Overlap
+          - "Diff Addr and Same Port" is allowed.
+          - "INADDR_ANY Addr and Same Port" is not allowed.
+        */
+        if ((*itr)->sockaddr != NULL && 
+          ((*itr)->sockaddr->sin_addr.s_addr == param_addr->sin_addr.s_addr || 
+          param_addr->sin_addr.s_addr == NL_INADDR_ANY || (*itr)->sockaddr->sin_addr.s_addr == NL_INADDR_ANY)) {
+          if ((*itr)->sockaddr->sin_port == param_addr->sin_port) {
+            returnSystemCall(syscallUUID, -1);
+          }
+        }
+
+        // Find same pid, same fd.
+        if ((*itr)->fd == param_fd) {
+          findItr = itr;
+          isFind = true;
+        }
+      }
+    }
+
+    // Prevent Closed sockets or unexisted sockets.
+    if (!isFind)
+      returnSystemCall(syscallUUID, -1);
+
+    struct sock_table_item* sock_table_item = *findItr;
+    // Prevent Double Bind. If it is already bound, return -1. ("Same addr and Diff port" is not allowed.)
+    if (sock_table_item->sockaddr != NULL) {
+      returnSystemCall(syscallUUID, -1);
+    }
+
+    struct sockaddr_in* addr = (struct sockaddr_in *) malloc(sizeof(struct sockaddr_in));
+    if (sock_table_item == NULL) {
+      printf("Error: can't allocate memory(sockaddr_in)\n");
+      returnSystemCall(syscallUUID, -1);
+    }
+
+    addr->sin_len = param_addr->sin_len;
+    addr->sin_family = param_addr->sin_family;
+    addr->sin_port = param_addr->sin_port;
+    addr->sin_addr.s_addr = param_addr->sin_addr.s_addr;
+    sock_table_item->sockaddr = addr;
+
+    returnSystemCall(syscallUUID, 0);
     break;
+  }
   case GETSOCKNAME:
     // this->syscall_getsockname(
     //     syscallUUID, pid, std::get<int>(param.params[0]),
